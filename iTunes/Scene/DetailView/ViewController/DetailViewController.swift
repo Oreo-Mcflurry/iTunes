1b600049f40332d944f7ea58f2859f67d2b4acaa
유인호 <yuiop1029@naver.com>
2024-04-07 21:35:51 +0900

    [ADD] Add DetailView

---
+++ iTunes/Scene/DetailView/ViewController/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.swift
+//  iTunes
+//
+//  Created by A_Mcflurry on 4/7/24.
+//
+
+import UIKit
+import SnapKit
+
+final class DetailViewController: BaseViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
---
+++ iTunes/Scene/DetailView/ViewModel/DetailViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  DetailViewModel.swift
+//  iTunes
+//
+//  Created by A_Mcflurry on 4/7/24.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
